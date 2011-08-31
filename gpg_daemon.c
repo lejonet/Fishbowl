5698d9eafc917caa334618914daae9758c5a732d
Daniel Kuehn <daniel@kuehn.se>
2011-08-31 12:57:48 +0200

    Fixed git screw up

--- gpg_daemon.c
+++ gpg_daemon.c
@@ -1,19 +1,21 @@
-// Fishbowl daemon - A small daemon to fetch, decrypt and verify, encrypt and move the files that arrives
-// Copyright (C) 2011 Daniel Kuehn <daniel@kuehn.se>
-// 
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+/*
+ Fishbowl daemon - A small daemon to fetch, decrypt and verify, encrypt and move the files that arrives
+ Copyright (C) 2011 Daniel Kuehn <daniel@kuehn.se>
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+*/
 
 #include <sys/types.h>
 #include <stddef.h>
@@ -28,17 +30,15 @@
 #define BUF_SIZE 4096
 // fail_if_err macro borrowed from the t-support.c file in the tests/gpg directory of the gpgme tarball
 #define fail_if_error(error)					\
-  do								\
-    {								\
-      if (error)						\
-        {							\
-          fprintf (stderr, "%s:%d: %s: %s\n",			\
+  do {								\
+    if (error) {						\
+          fprintf(stderr, "%s:%d: %s: %s\n",			\
                    __FILE__, __LINE__, gpgme_strsource (error),	\
 		   gpgme_strerror (error));			\
-          exit (1);						\
+          exit(1);						\
         }							\
-    }								\
-  while (0)
+  } while(0)							\
+
 
 //TODO: Function that encrypts the files with the fishbowl key and then moves them to another directory
 
@@ -56,9 +56,10 @@ struct node *search_directory(const char *);
 void add_element(struct node **, char *);
 void traverse_list(struct node *);
 struct gpg_data *decrypt_gpg(char *, char *, char *);
-// gpgme_data_t encrypt_gpg(gpgme_data_t, char *, char *);
+struct gpg_data *encrypt_gpg(struct gpg_data *, char *, char *);
 void init_gpgme(gpgme_protocol_t, char *, char *);
-void print_gpg_data(gpgme_data_t);
+void print_gpg_decrypt_data(gpgme_data_t);
+void print_gpg_verify_data(gpgme_ctx_t);
 
 int main(void) {
   struct node *ptr;
@@ -114,7 +115,7 @@ void traverse_list(struct node *list) {
   while (ptr != NULL && ptr->data !=NULL) {
       printf("Data: %s\n", ptr->data);
       gpg_outdata = decrypt_gpg(ptr->data, "/usr/bin/gpg", ".gnupg");
-      print_gpg_data(gpg_outdata->data);
+      print_gpg_decrypt_data(gpg_outdata->data);
       ptr = ptr->next;
   }
 }
@@ -143,15 +144,13 @@ struct gpg_data *decrypt_gpg(char *file, char *binpath, char *homedir) {
   return outdata;
 }
 
-//gpgme_data_t encrypt_gpg(gpgme_data_t *plaintext, char *binpath, char *homedir) {
-//  FILE *fd_out;
-//  gpgme_ctx_t ctx;
-//  gpgme_error_t error;
-//
-//  init_gpgme(GPGME_PROTOCOL_OpenPGP, binpath, homedir);
-//  gpgme_new(&ctx);
-//  gpgme_set_armor(ctx, 1);
-//}
+struct gpg_data *encrypt_gpg(struct gpg_data *indata, char *binpath, char *homedir) {
+  gpgme_error_t error;
+
+  init_gpgme(GPGME_PROTOCOL_OpenPGP, binpath, homedir);
+  gpgme_new(&indata->ctx);
+  gpgme_set_armor(indata->ctx, 1);
+}
 
 // init_gpgme code borrowed from t-support.c in the tests/gpg directory of the gpgme tarball with some own additions
 void init_gpgme (gpgme_protocol_t protocol, char *binpath, char *homedir) {
@@ -168,7 +167,7 @@ void init_gpgme (gpgme_protocol_t protocol, char *binpath, char *homedir) {
 }
 
 // Code borrowed from the print_data function in t-support.h of the gpgme-tarball with some own additions
-void print_gpg_data(gpgme_data_t data) {
+void print_gpg_decrypt_data(gpgme_data_t data) {
   char buf[BUF_SIZE+1];
   int res;
 
@@ -185,4 +184,10 @@ void print_gpg_data(gpgme_data_t data) {
 
   if (res < 0)
     fail_if_error(gpgme_err_code_from_errno(errno));
+}
+
+void print_gpg_verify_data(gpgme_ctx_t ctx) {
+  gpgme_signature_t signature;
+  
+  signature = gpgme_op_verify_result(
 }
