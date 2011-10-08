779300036d3e745efea93cf6fd86ae15ed1720f3
deadbyte <deadbyte.ph@gmail.com>
2011-10-08 19:41:50 +0200

    add function _log() for audit/error logging + macro log_audit/log_error; add function cleanup_fishbowl; rename function go_fishing to pickup_fishes and create a new go_fishing funtion which uses inotify; add support for daemonizing;

--- fishbowl.c
+++ fishbowl.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stddef.h>
 #include <errno.h>
 #include <gpgme.h>
@@ -8,27 +10,61 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/inotify.h>
+
+/*
+ * TODO
+ * parse config file
+ * parse argv
+ */
 
 #define BIN "/usr/bin/gpg"
-#define OUT "./TEST_OUT"
 #define KEYID "D09AFF79"
 #define CONFIG ".gnupg"
+#define FISHBOWL "./gpgtest"
+#define LEAKBOWL "./TEST_OUT"
+#define AUDIT_LOG "./fishbowl_audit.log"
+#define ERROR_LOG "./fishbowl_error.log"
 
-#define BUF_SIZE 4096
-#define fail_if_error(error) do {						\
-	if (error) {								\
-		fprintf(stderr, "%s:%d: %s: %s\n",				\
-				__FILE__, __LINE__, gpgme_strsource (error),	\
-				gpgme_strerror (error));			\
-		exit(EXIT_FAILURE);						\
-	}									\
+#define SIZE 4096
+#define log_audit(...) _log(fp_audit, __VA_ARGS__)
+#define log_error(...) _log(fp_error, __VA_ARGS__)
+#define fail_if_error(error, ret)				\
+do {								\
+	if (error) {						\
+		log_error("%s:%d: %s: %s\n",			\
+				__FILE__, __LINE__,		\
+				gpgme_strsource(error),		\
+				gpgme_strerror(error));		\
+		return (ret);					\
+	}							\
 } while(0)
 
 int armor = 1;
 char *bin = BIN;
 char *keyid = KEYID;
 char *config = CONFIG;
+char *fishbowl = FISHBOWL;
+char *leakbowl = LEAKBOWL;
+char *audit_log = AUDIT_LOG;
+char *error_log = ERROR_LOG;
+FILE *fp_audit, *fp_error;
+
 gpgme_ctx_t ctx;
+unsigned int count = 0;
+
+/*
+ *
+ */
+void _log (FILE *fp, char *format, ...)
+{
+	va_list list;
+
+	va_start(list, format);
+	vfprintf(fp, format, list);
+	va_end(list);
+	fflush(fp);
+}
 
 /*
  *
@@ -37,18 +73,18 @@ void write_file (gpgme_data_t *data, char *file)
 {
 	FILE *fp;
 	int count;
-	char buf[BUF_SIZE];
+	char buf[SIZE];
 
 	fp = fopen(file, "w");
 	if (!fp) {
-		perror("fopen failed");
+		log_error("fopen failed: `%s'\n", file);
 		return;
 	}
 
-	while ((count = gpgme_data_read(*data, buf, BUF_SIZE))) {
+	while ((count = gpgme_data_read(*data, buf, SIZE))) {
 
 		if (count == -1) {
-			perror("gpgme_data_read failed");
+			log_error("gpgme_data_read failed");
 			return;
 		}
 
@@ -69,18 +105,18 @@ gpgme_data_t *decrypt (char *file, gpgme_data_t *plain)
 
 	fp = fopen(file, "r");
 	if (!fp) {
-		perror("fopen failed");
+		log_error("fopen failed: `%s'\n", file);
 		return NULL;
 	}
 
 	error = gpgme_data_new_from_stream(&cipher, fp);
-	fail_if_error(error);
+	fail_if_error(error, NULL);
 
 	error = gpgme_data_new(plain);
-	fail_if_error(error);
+	fail_if_error(error, NULL);
 
 	error = gpgme_op_decrypt_verify(ctx, cipher, *plain);
-	fail_if_error(error);
+	fail_if_error(error, NULL);
 
 	fclose(fp);
 	gpgme_data_release(cipher);
@@ -98,13 +134,13 @@ gpgme_data_t *encrypt (gpgme_data_t *plain, gpgme_data_t *cipher)
 	gpgme_key_t rcpt[2] = {NULL, NULL};
 
 	error = gpgme_get_key(ctx, keyid, &rcpt[0], 0);
-	fail_if_error(error);
+	fail_if_error(error, NULL);
 
 	error = gpgme_data_new(cipher);
-	fail_if_error(error);
+	fail_if_error(error, NULL);
 
 	error = gpgme_op_encrypt_sign(ctx, rcpt, GPGME_ENCRYPT_ALWAYS_TRUST, *plain, *cipher);
-	fail_if_error(error);
+	fail_if_error(error, NULL);
 
 	gpgme_key_unref(rcpt[0]);
 	gpgme_data_release(*plain);
@@ -122,32 +158,33 @@ void catch_a_fish (char *path, char* name)
 	gpgme_data_t plain, cipher;
 
 	snprintf(fish, PATH_MAX, "%s/%s", path, name);
-	printf("New fish: %s\n", fish);
+	log_audit("New!   fish number %u: `%s'\n", ++count, fish);
 
 	decrypt(fish, &plain);
 	encrypt(&plain, &cipher);
 
-	snprintf(fish, PATH_MAX, OUT"/%s.gpg", name);
+	snprintf(fish, PATH_MAX, "%s/%s.gpg", leakbowl, name);
 	write_file(&cipher, fish);
+	log_audit("Moved! fish number %u: `%s'\n", count, fish);
 }
 
 /*
  *
  */
-void go_fishing (char *fishbowl)
+void pickup_fishes (char *fishbowl)
 {
 	DIR *dirp;
 	struct dirent *dentry;
 
 	dirp = opendir(fishbowl);
 	if (!dirp) {
-		fprintf(stderr, "opendir failed: `%s'\n", fishbowl);
+		log_error("opendir failed: `%s'\n", fishbowl);
 		return;
 	}
 
 	while ((dentry = readdir(dirp))) {
 
-		if (dentry->d_type == DT_DIR)
+		if (dentry->d_type != DT_REG)
 			continue;
 
 		catch_a_fish(fishbowl, dentry->d_name);
@@ -159,39 +196,109 @@ void go_fishing (char *fishbowl)
 /*
  *
  */
-void init_fishbowl (void)
+void go_fishing (char *fishbowl)
 {
+	int fd, ret;
+	char buf[SIZE];
+	struct inotify_event *event;
+
+	pickup_fishes(fishbowl);
+
+	fd = inotify_init();
+	if (fd < 0) {
+		log_error("inotify_init failed\n");
+		return;
+	}
+
+	ret = inotify_add_watch(fd, fishbowl, IN_CLOSE_WRITE|IN_MOVED_TO);
+	if (ret < 0) {
+		log_error("inotify_add_watch failed: `%s'\n", fishbowl);
+		return;
+	}
+
+	do {
+		memset(buf, 0, SIZE);
+		ret = read(fd, buf, SIZE);
+		if (ret < 1) {
+			log_audit("end of read, quitting\n");
+			break;
+		}
+
+		event = (struct inotify_event *)buf;
+		catch_a_fish(fishbowl, event->name);
+
+	} while (1);
+
+	close(fd);
+}
+
+/*
+ *
+ */
+int init_fishbowl (void)
+{
+	int ret;
 	gpgme_error_t error;
 
-	mkdir(OUT, 0700);
+	ret = mkdir(leakbowl, 0700);
+
+	fp_audit = fopen(audit_log, "a");
+	if (!fp_audit) {
+		log_error("fopen failed: `%s'\n", audit_log);
+		return 0;
+	}
+		
+	fp_error = fopen(error_log, "a");
+	if (!fp_error) {
+		log_error("fopen failed: `%s'\n", error_log);
+		return 0;
+	}
 
 	error = gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP);
-	fail_if_error(error);
+	fail_if_error(error, 0);
 
 	error = gpgme_set_engine_info(GPGME_PROTOCOL_OpenPGP, bin, config);
-	fail_if_error(error);
+	fail_if_error(error, 0);
 
 	gpgme_check_version(NULL);
 	setlocale(LC_ALL, "");
 	gpgme_set_locale(NULL, LC_CTYPE, setlocale(LC_CTYPE, NULL));
 
 	error = gpgme_new(&ctx);
-	fail_if_error(error);
+	fail_if_error(error, 0);
 
 	gpgme_set_armor(ctx, armor);
+
+	return 1;
 }
 
+/*
+ *
+ */
+void cleanup_fishbowl (void)
+{
+	gpgme_release(ctx);
+	fclose(fp_audit);
+	fclose(fp_error);
+}
 
 /*
  *
  */
 int main (void)
 {
-	init_fishbowl();
+	if (!init_fishbowl()) {
+		fprintf(stderr, "initialization failed, quitting\n");
+		exit(EXIT_FAILURE);
+	}
 
-	go_fishing("./gpgtest");
+	printf("going dark...\n");
 
-	gpgme_release(ctx);
+	daemon(1, 0);
+
+	go_fishing(fishbowl);
+
+	cleanup_fishbowl();
 
 	return 0;
 }
